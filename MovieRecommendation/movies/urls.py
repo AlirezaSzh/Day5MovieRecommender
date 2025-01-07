5a90435d5a826044328a6d58360370c1833713ad
Mathur Danduprolu <mathur.developer@gmail.com>
2025-01-06 21:24:55 -0600

    first commit

---
+++ MovieRecommendation/movies/urls.py
@@ -0,0 +1,8 @@
+# movies/urls.py
+from django.urls import path
+from .views import HomePageView, MovieDetailView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('movie/<int:movie_id>/', MovieDetailView.as_view(), name='movie_detail'),
+]
